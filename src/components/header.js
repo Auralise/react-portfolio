72b3e57294ca749a63528245fc8459b36c56601d
James Prince <james.prince1@gmail.com>
2023-01-09 13:33:34 +1100

    Removed the navigation from the header for a clearer separation of concerns. This may change back considering the header and navigation will be rendered on every page

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-//Import Nav as this is part of the header component
-import Navigation from "./navigation";
-
 const Header = () => {
     return (
-        <header className="header-wrapper">
+        <div className="header-text">
             <h1>James Prince</h1>
             <p className="sub-heading">Full-stack developer</p>
-
-            <Navigation />
-        </header>
+        </div>
     )
 }
 
