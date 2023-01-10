8c7205c388970dafb472c5cb5e1b2e9bc0c81f4f
James Prince <james.prince1@gmail.com>
2023-01-10 16:30:08 +1100

    Added contact form and various helpers to handle verification of email addresses and print an error message.

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,20 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { validateEmail, sendContactMessage } from "../../utils/helpers";
 
 const Contact = () => {
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === "name"){
+            setName(inputValue);
+        } else if (inputType ==="email") {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue)
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(name.length === 0) {
+            setErrorMessage("Please provide a name");
+            return;
+        } else if(!validateEmail(email)){
+            setErrorMessage("Please provide a valid email address");
+            return; 
+        } else if(message.length === 0) {
+            setErrorMessage("Please provide a message");
+            return;
+        }
+
+        sendContactMessage(name, email, message);
+
+        setName("");
+        setEmail("");
+        setMessage("");
+        setErrorMessage("");
+    }
+
     return (
-        <form> 
-            <input type="text" id="name" name="name" className="form-control"></input>
-            <label htmlFor="name" className="form-label">Name:</label>
+        
+        <form>
+            <label htmlFor="name" className="form-label">Name: </label>
+            <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                value={name} 
+                onClick={handleInputChange} 
+                className="form-control" 
+            />
 
-            <input type="text" id="email" name="email" className="form-control"></input>
-            <label htmlFor="email" className="form-label">Email:</label>
+            <label htmlFor="email" className="form-label">Email: </label>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                value={email} 
+                onChange={handleInputChange} 
+                className="form-control"
+            />
 
-            <textarea id="message" name="message" className="form-control"></textarea>
+            <label htmlFor="message" className="form-label">Message: </label>
+            <textarea 
+                id="message" 
+                name="message" 
+                className="form-control"
+                value={message} 
+                onChange={handleInputChange} 
+            />
 
-            <button type="submit" id="message-submit" className="btn btn-success">Submit</button>
+            { errorMessage && <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            }
+
+            <button 
+            type="button" 
+            id="message-submit" 
+            onClick={handleFormSubmit} 
+            className="btn btn-success">
+                Submit
+            </button>
         </form>
+        
     )
 
 }
---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
+
+export function sendContactMessage(name, email, message){
+    alert(`Logic not yet implemented as there is no back end yet. Details for the contact form are...\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+}
