e29c79828c5b31bcb1ddbe9992b40c0837fa6bc7
James Prince <james.prince1@gmail.com>
2023-01-10 14:09:23 +1100

    added about page with content

---
+++ src/components/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const About = () => {
+    return (
+        <section> 
+            <h2>About Me</h2>
+            <p>
+                I am a veteran of IT Support who is currently making a switch to a career as a developer. To facilitate this, I have undertaken a bootcamp course through Monash University, focusing specifically on the MERN (MongoDB, Express, React, NodeJS) stack.
+            </p>
+
+            <p>
+                I have an interest in programming in general and am experimenting with a variety of different languages such as Go and Rust. As the bootcamp that I am attending is teaching vanilla JavaScript, I have also been experimenting with TypeScript in my own time as well. In the past, I have also had some experience writing applications in C# and C++. 
+            </p>
+
+            <p>
+                Outside of programming, development and Computing in general, I also have an interests in woodworking, hiking, music and gaming. 
+            </p>
+        </section>
+    )
+}
+
+
+export default About;
