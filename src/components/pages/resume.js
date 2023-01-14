ac00d950e48514a4633fcf8eee8f1f2eaa61ac3e
James Prince <james.prince1@gmail.com>
2023-01-14 14:25:45 +1100

    Added basic page to serve an example resume file

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import resume from "./documents/resume_example.docx"
 
 const Resume = () => {
 
     return (
-        <div></div>
+        <div>
+            <p>Please find a copy of my resume here: </p>
+            <a href={resume} className="btn btn-primary" >Resume</a>
+        </div>
+
     )
 }
 
