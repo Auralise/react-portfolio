def71391f4c63eea66dac55bba4c5da803184101
James Prince <james.prince1@gmail.com>
2023-01-14 14:47:47 +1100

    Added profile picture and cleaned up a significant number of missing or mis-referenced files.

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,19 @@
 import React from "react";
+import profileImage from "./images/profile-placeholder.png";
 
 
 const About = () => {
+
+    const style = {
+        imageStyle: {
+            maxHeight: "250px",
+
+        }
+    }
+
     return (
         <section> 
+            <img src={profileImage} alt="My avatar" className="float-start m-2" style={style.imageStyle} />
             <h2>About Me</h2>
             <p>
                 I am a veteran of IT Support who is currently making a switch to a career as a developer. To facilitate this, I have undertaken a bootcamp course through Monash University, focusing specifically on the MERN (MongoDB, Express, React, NodeJS) stack.
